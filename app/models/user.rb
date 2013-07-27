b9184721d77fc4678753d257673e63c5f02c9c37
Alex Fusman <alex.fusman@gmail.com>
2013-07-27 17:22:21 -0500

    Add Device functionality - login, edit, logout

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # attr_accessible :title, :body
+  # MassAssignmentSecurity Error fix
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :as => [:default, :admin]
+  # Makes sure all users have names
+  validates_presence_of :name
+end
